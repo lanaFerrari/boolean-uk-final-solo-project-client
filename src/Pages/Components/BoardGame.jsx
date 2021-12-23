fdcad53886add81fd1d271a8f6cdf74cc70f2f2c
Lana Ferrari <ferrarirangel@gmail.com>
2021-12-23 13:46:24 +0000

    Deploy

--- src/Pages/Components/BoardGame.jsx
+++ src/Pages/Components/BoardGame.jsx
@@ -50,19 +50,21 @@ export default function BoardGame() {
     setSelectedPiece(null);
   }
 
+  function generateValidMoves(board, selectedPiece, color) {}
+
   return (
     <div className="centering">
       <div className="board">
         {board.map((row, indexRow) => {
           //   console.log("Row", row);
           return (
-            <div keindexRow={indexRow} className="row">
+            <div key={indexRow} className="row">
               {row.map((square, indexCol) => {
                 const position = { indexRow, indexCol, pieceId: square };
                 // console.log("square", square);
                 return (
                   <div
-                    keindexRow={indexCol}
+                    key={indexCol}
                     className={`square ${
                       (indexCol + indexRow) % 2 === 0
                         ? "square-white"
